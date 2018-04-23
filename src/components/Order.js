f60a8aad1f3f11cb7a68312ba3810c98a05481a1
Gray Smith <gray.smith00@gmail.com>
2018-04-23 13:06:39 -0600

    add inventory form for each fish and update fish functionality

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,10 @@ class Order extends React.Component {
   renderOrderItem = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    if (fish.status !== "available") {
+    const isAvailable = fish && fish.status === "available";
+    // make sure the fish is loaded before continuing
+    if (!fish) return null;
+    if (!isAvailable) {
       return (
         <li key={key}>
           Sorry {fish ? fish.name : "this fish"} is no longer available.
